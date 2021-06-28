eef217a71a9573206b302d729846aaff3bc7861c
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2021-06-28 11:06:43 -0300

    Add contagem de tentativas e tela de resultado no quiz

--- app/src/components/QuizOption.jsx
+++ app/src/components/QuizOption.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 
 const statusStyles = {
-  false: "text-gray-400",
+  false: "bg-gray-300 text-gray-500",
   true: "bg-green-400 text-white hover:bg-green-400",
 };
 
 const QuizOption = (props) => {
-  const { option, status } = props;
+  const { option, status, isSelecting, ...rest } = props;
 
-  const statusClass = statusStyles[status] || "";
+  const statusClasses = statusStyles[status] || "";
+  const isSelectingClasses = isSelecting ? "hover:bg-gray-300 cursor-pointer" : "";
 
   return (
     <li
-      {...props}
-      className={`w-full text-xl text-center p-5 my-4 text-black bg-gray-200 
-      transition-all hover:bg-gray-300 focus:outline-none 
-      cursor-pointer rounded-2xl select-none ${statusClass}`}>
+      {...rest}
+      className={`w-full text-xl text-center p-5 my-4 text-black bg-gray-100 transition-all 
+      rounded-2xl select-none ${statusClasses} ${isSelectingClasses}`}>
       {option}
     </li>
   );
--- app/src/pages/Quiz.jsx
+++ app/src/pages/Quiz.jsx
@@ -3,73 +3,106 @@ import { quizzes } from "../api/quizzes";
 
 import Button from "../components/Button";
 import QuizOption from "../components/QuizOption";
+import QuizResultView from "../components/QuizResultView";
 
 function Quiz(props) {
   const quiz = quizzes[props.quizIndex];
 
-  const [showResult, setShowResult] = useState(false);
+  const [attempts, setAttempts] = useState(1);
+  const [showAnswer, setShowAnswer] = useState(false);
+  const [showFinalResult, setShowFinalResult] = useState(false);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  const [selectedResult, setSelectedResult] = useState(false);
+  const [selectedOptionStatus, setSelectedOptionsStatus] = useState(false);
+  const [rightAnswersCount, setRightAnswersCount] = useState(0);
 
   function selectOption(optionIndex) {
-    if (quiz.questions[currentQuestionIndex].answerIndex === optionIndex) setSelectedResult(true);
-    else setSelectedResult(false);
+    if (showAnswer) return;
 
-    setShowResult(true);
-  }
+    if (quiz.questions[currentQuestionIndex].answerIndex === optionIndex) {
+      setSelectedOptionsStatus(true);
+      setRightAnswersCount(rightAnswersCount + 1);
+    } else {
+      setSelectedOptionsStatus(false);
+    }
 
-  function getOptionStatus(optionIndex) {
-    if (!showResult) return null;
-    return `${optionIndex === quiz.questions[currentQuestionIndex].answerIndex}`;
+    setShowAnswer(true);
   }
 
   function nextQuestion() {
     if (currentQuestionIndex < quiz.questions.length - 1) {
       setCurrentQuestionIndex(currentQuestionIndex + 1);
-      setShowResult(false);
+      setShowAnswer(false);
+    } else {
+      setShowFinalResult(true);
+      setShowAnswer(false);
     }
-    // else{} vai pra tela final de resultado do quiz
+  }
+
+  function getOptionStatus(optionIndex) {
+    if (!showAnswer) return null;
+    return `${optionIndex === quiz.questions[currentQuestionIndex].answerIndex}`;
+  }
+
+  function startAnotherAttempt() {
+    setAttempts(attempts + 1);
+    setShowAnswer(false);
+    setShowFinalResult(false);
+    setCurrentQuestionIndex(0);
+    setSelectedOptionsStatus(false);
+    setRightAnswersCount(0);
   }
 
   const backgroundFeedback = useMemo(() => {
-    if (!showResult) return "to-gray-500";
-    return selectedResult ? "to-green-500" : "to-red-600";
-  }, [showResult, selectedResult]);
+    if (!showAnswer) return "to-gray-500";
+    return selectedOptionStatus ? "to-green-500" : "to-red-600";
+  }, [showAnswer, selectedOptionStatus]);
 
   return (
     <div
       className={`min-h-screen w-full transition-all flex flex-col items-center p-8 bg-gradient-to-b from-gray-600 ${backgroundFeedback}`}>
       <header className="flex justify-between mb-14">
-        <h1 className="text-4xl font-bold">{quiz.title}</h1>
+        <h1 className="text-4xl font-bold">Quiz - {quiz.title}</h1>
       </header>
 
       <main className="flex flex-col w-full items-center">
-        <h2 className="text-2xl w-full text-center mb-5 text-gray-200 font-bold">
-          <span className="block mb-4 uppercase font-normal tracking-wider">
-            Pergunta ({currentQuestionIndex + 1}/{quiz.questions.length})
-          </span>
-          {quiz.questions[currentQuestionIndex].title}
-        </h2>
-
-        <ul className="flex flex-col justify-center mb-5 w-full max-w-lg">
-          {quiz.questions[currentQuestionIndex].options.map((option, i) => (
-            <QuizOption
-              key={i}
-              option={option}
-              status={getOptionStatus(i)}
-              onClick={() => selectOption(i)}
-            />
-          ))}
-        </ul>
-
-        {showResult && (
+        {showFinalResult ? (
+          <QuizResultView
+            totalQuestions={quiz.questions.length}
+            rightAnswers={rightAnswersCount}
+            startAnotherAttempt={startAnotherAttempt}
+            attempts={attempts}
+          />
+        ) : (
           <>
-            <span className=" text-white text-2xl rounded-lg p-4 mb-4">
-              {selectedResult ? "Resposta certa !" : " Resposta errada !"}
-            </span>
-            <Button color="blue" onClick={nextQuestion} className="">
-              Proxima pergunta
-            </Button>
+            <h2 className="text-2xl w-full text-center mb-5 text-gray-200 font-bold">
+              <span className="block mb-4 uppercase font-normal tracking-wider">
+                Pergunta ({currentQuestionIndex + 1}/{quiz.questions.length})
+              </span>
+              {quiz.questions[currentQuestionIndex].title}
+            </h2>
+
+            <ul className="flex flex-col justify-center mb-5 w-full max-w-lg">
+              {quiz.questions[currentQuestionIndex].options.map((option, i) => (
+                <QuizOption
+                  key={i}
+                  option={option}
+                  status={getOptionStatus(i)}
+                  isSelecting={!showAnswer}
+                  onClick={() => selectOption(i)}
+                />
+              ))}
+            </ul>
+
+            {showAnswer && (
+              <div className="w-full max-w-lg flex flex-col text-2xl  text-center text-white ">
+                <span className="rounded-lg p-4 mb-4">
+                  {selectedOptionStatus ? "Resposta certa !" : " Resposta errada !"}
+                </span>
+                <Button width="full" color="blue" onClick={nextQuestion} className="">
+                  Proxima pergunta
+                </Button>
+              </div>
+            )}
           </>
         )}
       </main>
