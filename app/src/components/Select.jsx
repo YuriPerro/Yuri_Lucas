4c2cab2a4f70e36122bea0b9358161a933ac931d
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2021-07-17 20:28:59 -0300

    Cria componente select padronizado com input

---
+++ app/src/components/Select.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Select(props) {
+  const { type = "text", options, className, ...rest } = props;
+
+  return (
+    <select
+      {...rest}
+      className={`mb-2 text-black p-2 px-2 w-full rounded-md bg-gray-100 focus:bg-white focus:outline-none focus:ring-4 ring-blue-400 ${className}`}>
+      <option value="" disabled>
+        Selecione...
+      </option>
+      {options.map((option) => (
+        <option key={option.text + option.value} value={option.value} disabled={option.disabled}>
+          {option.text}
+        </option>
+      ))}
+    </select>
+  );
+}
+
+export default Select;
