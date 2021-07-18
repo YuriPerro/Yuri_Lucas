051f0aeb64b63d5c646103594d40dfe45b44fe1b
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2021-07-18 12:40:05 -0300

    Refatoracao do service

--- app/src/api/services.ts
+++ app/src/api/services.ts
@@ -9,41 +9,43 @@ if (!firebase.apps.length) firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
 const auth = firebase.auth();
 
-export const addQuizTabela = async (quiz: QuizDataProps) => {
-  try {
-    const quizToDB = {
-      ...quiz,
-      ownerUID: auth.currentUser.uid,
-      createdAt: new Date().toString(),
-    };
-    const resp = await database.ref("quizes/").push(quizToDB);
-    if (resp) return true;
-  } catch (error) {
-    console.log(error);
-    return false;
-  }
-};
+export const API = {
+  addQuiz: async (quiz: QuizDataProps) => {
+    try {
+      const quizToDB = {
+        ...quiz,
+        ownerUID: auth.currentUser.uid,
+        createdAt: new Date().toString(),
+      };
+      const resp = await database.ref("quizes/").push(quizToDB);
+      if (resp) return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  },
 
-export const getQuizByType = async (type: string) => {
-  try {
-    const ref = database.ref(`quizes/`).orderByChild("type").equalTo(type);
-    const quiz = (await ref.once("value")).val();
-    if (quiz) return quiz;
-    else return {};
-  } catch (error) {
-    console.log(error);
-    return false;
-  }
-};
+  getQuizByType: async (type: string) => {
+    try {
+      const ref = database.ref(`quizes/`).orderByChild("type").equalTo(type);
+      const quiz = (await ref.once("value")).val();
+      if (quiz) return quiz;
+      else return {};
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  },
 
-export const getQuizById = async (quizUID: string) => {
-  try {
-    const ref = database.ref(`quizes/${quizUID}`);
-    const quiz = (await ref.once("value")).val();
-    if (quiz) return quiz;
-    else return {};
-  } catch (error) {
-    console.log(error);
-    return false;
-  }
+  getQuizById: async (quizUID: string) => {
+    try {
+      const ref = database.ref(`quizes/${quizUID}`);
+      const quiz = (await ref.once("value")).val();
+      if (quiz) return quiz;
+      else return {};
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  },
 };
