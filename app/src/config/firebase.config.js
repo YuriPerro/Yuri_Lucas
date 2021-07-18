460856b8f546c06f1371829355494646c6b2fb1c
Yuri Perro <yuribaumgartner@ice.ufjf.br>
2021-07-18 09:24:49 -0300

    remove ignore firebase

---
+++ app/src/config/firebase.config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCCQ6efUSBrSpArGvsodqiDXAPC2A-Rwrg",
+  authDomain: "project-quizzer.firebaseapp.com",
+  databaseURL: "https://project-quizzer-default-rtdb.firebaseio.com",
+  projectId: "project-quizzer",
+  storageBucket: "project-quizzer.appspot.com",
+  messagingSenderId: "515499470205",
+  appId: "1:515499470205:web:b9a78f7dbd2485aa213014",
+};
