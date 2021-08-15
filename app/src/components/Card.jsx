94079d61f79cdecf228558f83c56be589ba4fa57
Yuri Perro <yuribaumgartner@ice.ufjf.br>
2021-08-15 16:42:37 -0300

    Adiciona contagem de quiz vindo do banco no perfil

--- app/src/components/Card.jsx
+++ app/src/components/Card.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BadgeCheckIcon } from "@heroicons/react/outline";
 
 function Card(props) {
   const { quiz, isAcomplished, footer, hoverEffect, ...rest } = props;
@@ -35,20 +36,28 @@ function Card(props) {
       {...rest}
       className={`flex flex-col flex-1 border-b-8 border-${
         getColorByDiff()?.color
-      } bg-gray-100 shadow-lg rounded-md p-4 w-full ${
-        isAcomplished && "opacity-50"
-      } text-black transition-all transform ${hoverClasses}`}>
-      <div className="flex flex-row ">
-        <span className="text-xl mb-4 font-bold text-gray-700">{quiz.title}</span>
-        <div
-          className={`w-16 h-6 rounded-xl p-2 flex absolute right-4 top-4 flex-row items-center justify-center bg-${
-            getColorByDiff()?.color
-          }`}>
-          <span className="text-sm font-semibold text-gray-50">{getColorByDiff()?.name} </span>
+      } bg-gray-100 shadow-lg rounded-md  w-full text-black transition-all transform ${hoverClasses}`}>
+      <div className="p-4">
+        <div className="flex flex-row ">
+          <span className="text-xl mb-4 font-bold text-gray-700">{quiz.title}</span>
+          <div
+            className={`w-16 h-6 rounded-xl p-2 flex absolute right-4 top-4 flex-row items-center justify-center bg-${
+              getColorByDiff()?.color
+            }`}>
+            <span className="text-sm font-semibold text-gray-50">{getColorByDiff()?.name} </span>
+          </div>
         </div>
+        <p className="text-base mb-4 flex-1">{quiz.description}</p>
+
+        <span className="text-base text-gray-500">{footer}</span>
       </div>
-      <p className="text-base mb-4 flex-1">{quiz.description}</p>
-      <span className="text-base text-gray-500">{footer}</span>
+
+      {isAcomplished && (
+        <div className="flex flex-row items-center w-full bg-red-600">
+          <BadgeCheckIcon className="w-6 mr-1 text-blue-700" />
+          <span className="text-xs font-semibold text-blue-700">Quiz realizado</span>
+        </div>
+      )}
     </div>
   );
 }
