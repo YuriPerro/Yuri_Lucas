a48866abdadad2abc06d35bfc595cbeaa07d8814
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2021-08-30 23:16:12 -0300

    Add select test

---
+++ app/src/components/Select.test.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { mount } from "@cypress/react";
+import Select from "./Select";
+import Wrapper from "./Wrapper";
+import "../styles/index.css";
+
+it("Select should renders correctly", () => {
+  mount(
+    <Wrapper>
+      <Select
+        data-test="select"
+        options={[
+          { text: "Opção 1", value: "Opção 1" },
+          { text: "Opção 2", value: "Opção 2" },
+          { text: "Opção 3", value: "Opção 3" },
+        ]}
+        className="text-black">
+        Select
+      </Select>
+    </Wrapper>,
+  );
+
+  cy.get("[data-test='select']").should("exist");
+});
+
+it("Select should not renders is loading is false", () => {
+  mount(
+    <Wrapper>
+      <Select
+        data-test="select"
+        options={[
+          { text: "Opção 1", value: "Opção 1" },
+          { text: "Opção 2", value: "Opção 2" },
+          { text: "Opção 3", value: "Opção 3" },
+          { text: "Opção 4", value: "Opção 4" },
+          { text: "Opção 5", value: "Opção 5" },
+        ]}
+        className="text-black">
+        Select
+      </Select>
+    </Wrapper>,
+  );
+
+  cy.get("[data-test='select']").select("Opção 1");
+  cy.get("[data-test='select']").select("Opção 2");
+  cy.get("[data-test='select']").select("Opção 3");
+  cy.get("[data-test='select']").select("Opção 4");
+  cy.get("[data-test='select']").select("Opção 5");
+});
